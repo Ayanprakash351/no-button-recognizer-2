1efdd9dae54fc1e34d08cb16c52b0b35a3bacbe1
Ayanprakash351 <74969381+Ayanprakash351@users.noreply.github.com>
2021-08-17 15:49:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ function modelLoaded(){
 
 function gotResults(error, results){
     if(error){
-        console.error(error);
+        console.error(error)
     }
   else{
       console.log(results);
       document.getElementById("result_object_name").innerHTML = results[0].label;
-      document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
   }  
 }
